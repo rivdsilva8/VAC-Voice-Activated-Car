09e101b10457f69a85f2ea39f19fae5decd63f6b
rivaldo dsilva <rivaldodsilva8@gmail.com>
2024-10-12 22:47:09 -0400

    initial commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,7 @@
+import { VoiceControl } from "./VoiceControl";
+
+function App() {
+  return <div className="bg-stone-800 text-white h-screen"><VoiceControl/></div>;
+}
+
+export default App;
---
+++ Frontend/src/VoiceControl.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const VoiceControl = () => {
+  return (
+    <div className="flex flex-row justify-center pt-5">
+      <h1 className="text-4xl">VoiceControl</h1>
+    </div>
+  );
+};
